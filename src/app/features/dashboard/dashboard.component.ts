a55a2f42b18599029cdc46479a4f08e38b6c1a0a
linhnguyen3696 <linhnguyen3696@gmail.com>
2018-05-13 15:23:08 +0700

    finished social overview dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,14 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var jQuery: any;
+declare var $ :any;
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
+
+  overview: any;
+  overviewProgress: any;
+  overviewNumber: any;
+  overviewDesc: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.overview = [];
+  }
+
+  ngAfterViewInit() {
+    let name = ['Facebook', 'Twitter', 'Instagram', 'Google'];
+    let percent = [70.1, 40.5, 76.3, 54.9];
+    let number = [7842900, 180200, 38900, 3988];
+    let icon = ['facebook-f', 'twitter', 'instagram', 'google'];
+
+    let length = percent.length;
+    for(let i = 0; i < length; i++) {
+      let str = number[i].toLocaleString('en-us', {minimumFractionDigits: 0});
+      let obj = {
+        name: name[i],
+        percent: percent[i],
+        number: str,
+        icon: icon[i]
+      }
+      this.overview.push(obj);
+    }
+    // console.log(this.overview);
+    // this.overviewProgress = $('.overview-progress');
+    // this.overviewNumber = $('.overview-data-number');
+    // let progressLength = this.overviewProgress.length;
+    // for(let i = 0; i < progressLength; i++) {
+    //   this.overviewProgress[i].style.width = percent[i] + '%';
+    //   let temp = number[i].toLocaleString('en-us', {minimumFractionDigits: 0});
+    //   this.overviewNumber[i].innerHTML = temp;
+    // }
   }
 
 }
